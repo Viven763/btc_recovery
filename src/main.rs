a394f244d680026d2a40be997dc6ce1952e6274f
George Tuskaev <viven260@gmail.com>
2025-12-07 15:23:12 +0300

    fix: cargo

--- src/main.rs
+++ src/main.rs
@@ -360,6 +360,7 @@ fn run_gpu_worker(db: &mut Database) -> Result<(), Box<dyn std::error::Error>> {
                 .arg(chunk_size as u32)
                 .global_work_size(global_work_size)
                 .local_work_size(local_work_size)
+                .queue(queue.clone())
                 .build()?;
 
             match unsafe { kernel.enq() } {
